32610a355739e4445efb9b354424ba29aed56050
GozaRuu <kais.sghari@gmail.com>
2020-08-09 15:19:28 +0200

    add type files

---
+++ dist/data-struct-ext/winuser.h.d.ts
@@ -0,0 +1,6 @@
+/** https://docs.microsoft.com/zh-cn/windows/win32/api/winuser/ns-winuser-rid_device_info */
+export declare const RID_DEVICE_INFO: {
+    cbSize: string;
+    dwType: string;
+    DUMMYUNIONNAME: any;
+};
---
+++ dist/lib/shared.d.ts
@@ -0,0 +1,2 @@
+import * as _UnionDi from 'ref-union-di';
+export declare const Union: _UnionDi;
